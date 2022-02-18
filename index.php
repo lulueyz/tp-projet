54da95c91546739a3015f05acd6cea8a3817ead5
Thierno <sa.thierno@gmail.com>
2022-02-18 11:44:27 +0100

    ajout new files

--- index.php
+++ index.php
@@ -6,13 +6,14 @@ include_once 'config.php';
 include_once 'functions.php';
 
 
+
 include_once 'models/SimpleOrm.php';
-$conn = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD);
+$conn = new mysqli("localhost", "root", " ");
 
 if ($conn->connect_error)
 die(sprintf('Unable to connect to the database. %s', $conn->connect_error));
 
-SimpleOrm::useConnection($conn, DATABASE_NAME);
+SimpleOrm::useConnection($conn,'projet_php');
 
 
 
